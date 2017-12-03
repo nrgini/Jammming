24b129bb047bc84c148412d39185ba7dab77ac0a
nrgini <ncorwyn@gmail.com>
2017-12-03 17:16:34 -0600

    Update TrackList.js
    Added a map method on line 8 - 10.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,7 +5,9 @@ class TrackList extends React.Component {
     render() {
         return 
         <div className="TrackList">
-            <!-- You will add a map method that renders a set of Track components  -->
+            track={this.props.tracks.map(track => {
+                     return this.props.track.name, this.props.track.artist, this.props.track.album;
+                 })}
         </div>
     }
 }
