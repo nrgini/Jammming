520811adb84ee713d7d72803ceb1cebc66150576
nrgini <ncorwyn@gmail.com>
2017-12-05 18:27:57 -0600

    Update TrackList.js

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import './Track';
 import './TrackList.css';
 
 class TrackList extends React.Component {
     render() {
-        return 
-        <div className="TrackList">
-            track={this.props.tracks.map(track => {
-                     return this.props.track.name, this.props.track.artist, this.props.track.album;
-                 })}
-        </div>
+        return (
+            <div className="TrackList"> 
+                {this.props.tracks.map(track => {
+                    return <Track track={track}
+                        key={track.id}
+                        onAdd={this.props.onAdd}
+                        addTrack={this.props.track}
+                        onRemove={this.props.onRemove} 
+                        />
+                    })
+                }
+            </div>
+        );
     }
 }
 
