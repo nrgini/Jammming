67df1830d4552bf57ceacbbdb9afda15ca8ed685
nrgini <ncorwyn@gmail.com>
2017-12-05 18:26:43 -0600

    Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './../SearchBar';
-import './../SearchResults';
-import './../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searchResults: [{
+    this.state = { 
+      searchResults: [{
       name: 'Send Your Love',
       artist: 'Sting',
       album: 'Sacred Love'
@@ -17,17 +18,51 @@ class App extends React.Component {
       name: 'You Can Close Your Eyes',
       artist: 'James Taylor',
       album: 'Mud Slide Slim'
-    ,
+    },
     {
       name: 'A Case of You',
       artist: 'Joni Mitchell',
       album: 'Miles of Aisles'
     }
-  ]
-     
+  ],
+    playlistName: 'Random list',
+    plalistTracks: [{
+      name: 'Animus Vox',
+      artist: 'The Glitch Mob',
+      album: 'Drink the Sea'
+    },
+    {
+      name: 'Chandelier',
+      artist: 'Sia',
+      album: '1000 Forms of Fear'
+    },
+    {
+      name: 'Wild Thoughts',
+      artist: 'DJ Khaled',
+      album: 'Grateful'
     }
-    
+      ]
+    }
   }
+
+  addTrack(track){
+    if(!this.state.playlistTracks.includes(track)) {
+      let updatedplaylist = this.state.playlistTracks;
+      updatedplaylist.push(track);
+      this.setState({playlistTracks: updatedplaylist});
+    };
+      this.addTrack = this.addTrack.bind(this);
+  }
+
+  removeTrack(track) {
+    if(this.state.playlistTracks.includes(track)) {
+      let updatedplaylist = this.state.playlistTracks;
+      updatedplaylist.slice(track);
+      this.setState({playlistTracks: updatedplaylist});
+  }
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
   render() {
     return (
       <div>
@@ -43,10 +78,10 @@ class App extends React.Component {
         <div>
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            /*-- Add a SearchBar component --*/
+            <SearchBar />
             <div className="App-playlist">
-              <SearchResults searchResults={this.state.searchResults} />
-              /*-- Add a Playlist component --*/
+              <SearchResults searchResults={this.state.searchResults} onAdd={this.state.addTrack} />
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
             </div>
           </div>
         </div>
